9ee511d728396932588f7d021af410ce28364df2
Sean Mao <92931266+eigenwulf@users.noreply.github.com>
2024-10-31 00:05:03 +0000

    git commit -m

--- frontend/src/components/CustomizeCard.js
+++ frontend/src/components/CustomizeCard.js
@@ -1,29 +1,70 @@
 import React from "react";
 import { useState } from "react";
-import { Avatar, Center, Container, Box, Button, Flex, Grid, GridItem, Heading, HStack, Image, ListItem, SimpleGrid, Stack, VStack, Text } from "@chakra-ui/react";
+import { Avatar, Center, Container, Box, Button, Flex, Grid, GridItem, Heading, HStack, Image, Input, ListItem, SimpleGrid, Stack, VStack, Text } from "@chakra-ui/react";
 
 import { useGalleryContext } from "../store/GalleryContext";
 import Card_Example_blank from '../images/Card_Example_blank.png';
 
 function CustomizeCard() {
-    const [show, setShow] = useState(true);
+
     const { selectedCard } = useGalleryContext();
+    const [isEditing, setIsEditing] = useState(false);
+    const [text, setText] = useState('Click to edit this text');
+
+    const handleTextClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleInputChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const handleBlur = () => {
+    setIsEditing(false);
+    };
+
+
     return(
         <>
-<SimpleGrid  height={[100, 500 ]} columns={[1, 2]} spacing={0} mx='2'>
-	<Box  border='1px'>
-	<Center boxWidth='100%' pt='3em'>
+<SimpleGrid  height={[100, 600 ]} columns={[1, 2]} spacing={0} mx='2'>
+	<Box position='relative' border='1px' p='1' m='1'>
+	<Center boxSize='100%'>
 		<Image
-			width='100%'
 			borderRadius='lg'
-			// objectFit='cover'
 			src={Card_Example_blank}
-			alt='Landing'/>
+			alt='Card'/>
+        {isEditing ? (
+            <Input 
+                value={text}
+                onChange={handleInputChange}
+                onBlur={handleBlur}
+                position='absolute'
+                top='50%'
+                left='50%'
+                transform="translate(-50%, -50%)"
+                zIndex={1}
+                />
+        ) : (
+            <VStack position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" zIndex={1}>
+                <Text 
+                onClick={handleTextClick} 
+                cursor="pointer"
+                fontSize="lg" 
+                padding="2" 
+                borderRadius="md">
+                    {text}
+                </Text>
+            </VStack>
+        )
+    
+    }
+
+
 	</Center>
 	</Box>
 	{/* as prop: gives the chakra component the property of the 'as' HTML tag */}
 
-	<Box border='1px'>
+	<Box border='1px' p='1' m='1'>
 		<Center boxWidth='100%' pt='3em'>
 		<Image
 			width='100%'
@@ -35,32 +76,65 @@ function CustomizeCard() {
 	</Box>
 </SimpleGrid>
 
-
-        <Box height='500px' m='2' borderBottom='1px' borderColor='gray.200'
+    <Box height='200px' m='2' borderBottom='1px' borderColor='gray.200'
         // bgImage={vintage-paper-03}
         bgImage="url('../images/backgrounds/vintage-paper-04.jpg')"
         bgPosition="center"
         bgRepeat="repeat">
-        <p>
-        {(selectedCard !==null) ? (<h2> Selected Card Id: {selectedCard} </h2>)  :
-        (<h2>No image selected</h2>) }
-        </p>
-
 
         <Center>
             <Button 
             colorScheme='purple' 
             size={['sm','md']} 
             w='100px' 
-            mt='1em' 
-            onClick={() => setShow(!show)}>
-            Customize</Button>
-            {show ? <Box h='50px' border='1px'/> : null}
+            mt='1em' />
 
         </Center>
     </Box>
+    <p>
+        {(selectedCard !==null) ? (<h2> Selected Card Id: {selectedCard} </h2>)  :
+        (<h2>No image selected</h2>) }
+        </p>
     </>
     );
 }
 
-export default CustomizeCard;
+const EditableImageText = () => {
+    const [texts, setTexts] = useState([
+        'Editable Text 1',
+        'Editable Text 2',
+        'Editable Text 3',
+        'Editable Text 4',
+    ]);
+
+    const handleTextChange = (index, newText) => {
+        const updatedTexts = [...texts];
+        updatedTexts[index] = newText;
+        setTexts(updatedTexts);
+    };
+
+    return(
+    <SimpleGrid  height={[100, 600 ]} columns={[1, 2]} spacing={0} mx='2'>
+	<Box position='relative' border='1px' p='1' m='1'>
+        <Center boxSize='100%'>
+            <Image
+                borderRadius='lg'
+                src={Card_Example_blank}
+                alt='Card'/>
+        </Center>
+        <VStack position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" zIndex={1} spacing={4}>
+            {texts.map( (text, index) => (
+            <EditableText
+                key={index}
+                text={text}
+                onTextChange={(newText) => handleTextChange(index, newText)}
+            />
+                        )
+                    )  
+                }
+        </VStack>
+    </Box>
+
+    </SimpleGrid>
+    );
+};
